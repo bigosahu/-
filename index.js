29e9c2a1d3ff85eac893bc8ce5165135f84a3806
bigosahu <momohjoy259@gmail.com>
2024-11-08 12:41:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('Bot is running!'));
+
+app.listen(3000, () => console.log('Server is live!'));
 const TelegramBot = require('node-telegram-bot-api');
 
 // Replace with your actual bot token from BotFather
